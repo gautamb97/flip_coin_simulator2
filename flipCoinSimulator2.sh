f20331fdee1b58317cb8f4059559370b35c61341
icode32 <www.gautam971997@gmail.com>
2020-11-10 00:59:38 +0530

    MODIFIED:Either_One_of_Them_Won_21_Times

--- flipCoinSimulator2.sh
+++ flipCoinSimulator2.sh
@@ -4,20 +4,39 @@ echo "WELCOME TO FLIP COIN SIMULATOR2"
 
 HEADWON=0;
 TAILWON=0;
+MAX_COUNT=21;
 
-for (( i=0; i<10; i++))
+while [ $HEADWON -le 21 ] && [ $TAILWON -le 21 ]
 do
 flipCoinCheck=$((RANDOM%2))
 
 if [ $flipCoinCheck -eq 0 ]
 then
         echo "HEADS";
-	HEADWON=$((HEADS++))
+	HEADWON=$((HEADWON+1))
 else
         echo "TAILS";
-	TAILWON=$((TAILS++))
+	TAILWON=$((TAILWON+1))
 fi
 done
 
 echo $HEADWON "NUMBER OF TIMES HEAD WON";
 echo $TAILWON "NUMBER OF TIMES TAIL WON";
+
+if [ $HEADWON -eq 21 ]
+then
+ 	echo "HEADS WON";
+elif [ $TAILWON -eq 21 ]
+then
+	echo "TAILS WON";
+else
+	echo "TIE"
+fi
+if [ $HEADWON -gt 21 ]
+then
+	diff=$(($HEADWON-$TAILWON))
+	echo $diff "NUMBER OF TIMES HEAD WON";
+else 
+	diff=$(($TAILWON-$HEADWON))
+	echo $diff "NUMBER OF TIMES TAIL WON";
+fi
