9827997a5c59da16069b35fb3d1e66187ca0e2d2
icode32 <www.gautam971997@gmail.com>
2020-11-09 23:47:11 +0530

    Start_With_Heads_or_Tails

--- flipCoinSimulator2.sh
+++ flipCoinSimulator2.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "WELCOME TO FLIP COIN SIMULATOR2"
+
+#checking HEADS OR TAILS
+flipCoinCheck=$((RANDOM%2))
+
+if [ $flipCoinCheck -eq 0 ]
+then
+        echo "HEADS";
+else
+        echo "TAILS";
+fi
