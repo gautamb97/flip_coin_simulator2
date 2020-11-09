55da5fa5f9c2ed4c1cd75679dd9e232a3d98ef28
icode32 <www.gautam971997@gmail.com>
2020-11-10 00:17:08 +0530

    MODIFIED:Number_of_Heads_and_Tails_Won

--- flipCoinSimulator2.sh
+++ flipCoinSimulator2.sh
@@ -2,12 +2,22 @@
 
 echo "WELCOME TO FLIP COIN SIMULATOR2"
 
-#checking HEADS OR TAILS
+HEADWON=0;
+TAILWON=0;
+
+for (( i=0; i<10; i++))
+do
 flipCoinCheck=$((RANDOM%2))
 
 if [ $flipCoinCheck -eq 0 ]
 then
         echo "HEADS";
+	HEADWON=$((HEADS++))
 else
         echo "TAILS";
+	TAILWON=$((TAILS++))
 fi
+done
+
+echo $HEADWON "NUMBER OF TIMES HEAD WON";
+echo $TAILWON "NUMBER OF TIMES TAIL WON";
